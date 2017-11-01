f5b69b77d3afaa140d72e436252a36a774000518
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2017-11-01 16:26:51 +0300

    Add files via upload

---
+++ cKupeli/workspace/Donkey Kong/src/source/Main.java
@@ -0,0 +1,19 @@
+package source;
+
+import java.io.FileNotFoundException;
+
+public class Main implements Runnable{
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
