cfe7deb9f3fdc3ff2bb6d68eb445b7eaef005e03
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2017-11-02 16:37:14 +0300

    Update GamePanel.java

--- cKupeli/workspace/Donkey Kong/src/source/GamePanel.java
+++ cKupeli/workspace/Donkey Kong/src/source/GamePanel.java
@@ -1,8 +1,15 @@
 package source;
 
+import java.awt.event.ActionEvent;
 import java.io.FileNotFoundException;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 public class GamePanel extends JPanel implements Runnable{
 	private Thread myThread;
@@ -12,8 +19,17 @@ public class GamePanel extends JPanel implements Runnable{
 	public GamePanel(int level) throws FileNotFoundException {
 		gameEngine = new GameEngine(level);
 		
-		myThread = new Thread(this);
-		myThread.start();
+		/*
+		 * start() generates thread for GamePanel class.
+		 * I made a method because of I might want to use structure of this class again in some other class such as PauseMenu.
+		 */
+		start();
+		
+		/*
+		 * initializeKeyBindings() decides which keyboard inputs we are going to use and as in the name initialize them
+		 * Same reason as start() method. I might want to use KeyHandler inner class and this method in other GUI parts such as MainMenu.
+		 */
+		initializeKeyBindings();
 	}
 	
 	@Override
@@ -21,24 +37,10 @@ public class GamePanel extends JPanel implements Runnable{
 		// TODO Auto-generated method stub
 		while(!gameEngine.isGameOver()){ //While game is not over
 			if(gameEngine.isMovement()){ //While game is running
-				/* KeyListeners will be added later
-				 * There will be another if statement in this segment of the code.
-				 * When the user pressed appropriate KeyListeners, the code will call pause() method.
-				 * Thus, it will set the movement false and the loop won't execute this segment of the code.
-				 * if(keyListener pressed){
-				 *	 pause();
-				 * }
-				 */
+				//Game algorithm
 			}
 			else if(!gameEngine.isMovement()){ //While game is paused
-				/* KeyListeners will be added later
-				 * There will be another if statement in this segment of the code.
-				 * When the user pressed appropriate KeyListeners, the code will call resume() method.
-				 * Thus, it will set the movement true and the loop won't execute this segment of the code.
-				 * if(keyListener pressed){
-				 *	 resume();
-				 * }
-				 */
+				//Pause menu initialization if needed
 			}
 			
 			try{
@@ -51,15 +53,95 @@ public class GamePanel extends JPanel implements Runnable{
 		}
 	}
 	
-	public void pause(){
+	//Nested class to implement KeyBindings
+	class KeyHandler extends AbstractAction{
+		String name;
+		
+		public KeyHandler(String name){
+			this.name = name;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(name == "escape"){
+				System.out.println("Escape Pressed");
+				if(gameEngine.isMovement()){ //While game is running
+					pause();
+					System.out.print("Movement is ");
+					System.out.println(gameEngine.isMovement());
+				}
+				else if(!gameEngine.isMovement()){ //While game is paused
+					resume();
+					System.out.print("Movement is ");
+					System.out.println(gameEngine.isMovement());
+				}
+			}
+			else if(name == "w"){
+				System.out.println("W Pressed");
+			}
+			else if(name == "a"){
+				System.out.println("A Pressed");
+			}
+			else if(name == "s"){
+				System.out.println("S Pressed");
+			}
+			else if(name == "d"){
+				System.out.println("D Pressed");
+			}
+			
+		}
+		
+	}
+	
+	public void initializeKeyBindings(){
+		InputMap inputMapPause = this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		ActionMap actionMapPause = this.getActionMap();
+		
+		/*
+		 * 1st parameter is the name of the keyboard input.
+		 * ESCAPE = ESC button
+		 * W = W button
+		 * A = A button
+		 * S = S button
+		 * D = D button
+		 * 2nd parameter is the name of the action which indicates pressing a button which we will use with ActionMap object.
+		 */
+		
+		/* 
+		 * I decided to use release for ESCAPE to prevent overlapping.
+		 * I want player to press once to pause or resume the game and released ia a perfect fit for that action.
+		 */
+		inputMapPause.put(KeyStroke.getKeyStroke("released ESCAPE"), "escapePressed");
+		actionMapPause.put("escapePressed", new KeyHandler("escape"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("W"), "wPressed");
+		actionMapPause.put("wPressed", new KeyHandler("w"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("A"), "aPressed");
+		actionMapPause.put("aPressed", new KeyHandler("a"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("D"), "dPressed");
+		actionMapPause.put("dPressed", new KeyHandler("d"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("S"), "sPressed");
+		actionMapPause.put("sPressed", new KeyHandler("s"));
+	}
+	
+	private void start(){
+		myThread = new Thread(this);
+		myThread.start();
+	}
+	
+	private void pause(){
 		gameEngine.setMovement(false);
 	}
 	
-	public void resume(){
+	private void resume(){
 		gameEngine.setMovement(true);
 	}
 	
-	public void stop(){
+	private void stop(){
 		gameEngine.setGameOver(true);
 	}
 	
