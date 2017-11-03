5c8f474d138384ae8e60a155063790b610a86ea8
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2017-11-03 09:11:28 +0300

    Update GamePanel.java

--- cKupeli/workspace/Donkey Kong/src/source/GamePanel.java
+++ cKupeli/workspace/Donkey Kong/src/source/GamePanel.java
@@ -1,10 +1,11 @@
 package source;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.io.FileNotFoundException;
 
 import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.ActionMap;
 import javax.swing.InputMap;
 import javax.swing.JComponent;
@@ -13,23 +14,26 @@ import javax.swing.KeyStroke;
 
 public class GamePanel extends JPanel implements Runnable{
 	private Thread myThread;
-	
 	private GameEngine gameEngine;
+	//private static int score = 0;
 
-	public GamePanel(int level) throws FileNotFoundException {
+	public GamePanel(int level) throws FileNotFoundException{
+		init(level);
+	}
+	
+	public void init(int level) throws FileNotFoundException{
 		gameEngine = new GameEngine(level);
-		
 		/*
 		 * start() generates thread for GamePanel class.
-		 * I made a method because of I might want to use structure of this class again in some other class such as PauseMenu.
+		 * I made a method because of we might want to use structure of this class again in some other class such as PauseMenu.
 		 */
 		start();
 		
 		/*
 		 * initializeKeyBindings() decides which keyboard inputs we are going to use and as in the name initialize them
-		 * Same reason as start() method. I might want to use KeyHandler inner class and this method in other GUI parts such as MainMenu.
+		 * Same reason as start() method. We might want to use KeyHandler inner class and this method in other GUI parts such as MainMenu.
 		 */
-		initializeKeyBindings();
+		initializingKeyBindings();
 	}
 	
 	@Override
@@ -37,10 +41,11 @@ public class GamePanel extends JPanel implements Runnable{
 		// TODO Auto-generated method stub
 		while(!gameEngine.isGameOver()){ //While game is not over
 			if(gameEngine.isMovement()){ //While game is running
-				//Game algorithm
+				//Draw the game
 			}
 			else if(!gameEngine.isMovement()){ //While game is paused
 				//Pause menu initialization if needed
+				//Draw pause menu or go pause menu, depends on initialization
 			}
 			
 			try{
@@ -50,7 +55,63 @@ public class GamePanel extends JPanel implements Runnable{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			repaint();
 		}
+		//Go back to main menu
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 * 
+	 * When we call repaint() in overriden run() method, it calls paintComponent() method and render the JPanel.
+	 * This method initialize after object methods because we need to call drawImage by using those objects.
+	 */
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		//Test
+		g.fillRect(0, 0, 500, 500);
+		g.setColor(Color.BLACK);
+		g.drawString("TEST", (int)(Math.random()*(600-500)+500), (int)(Math.random()*(600-500)+500));
+	}
+	
+	public void initializingKeyBindings(){
+		InputMap inputMapPause = this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		ActionMap actionMapPause = this.getActionMap();
+		
+		/*
+		 * 1st parameter is the name of the keyboard input.
+		 * ESCAPE = ESC button
+		 * W = W button
+		 * A = A button
+		 * S = S button
+		 * D = D button
+		 * SPACE = Space Button
+		 * 2nd parameter is the name of the action which indicates pressing a button which we will use with ActionMap object.
+		 */
+		
+		/* 
+		 * We decided to use release for ESCAPE to prevent overlapping.
+		 * We want player to press once to pause or resume the game and released is a perfect fit for that action.
+		 */
+		inputMapPause.put(KeyStroke.getKeyStroke("released ESCAPE"), "escapePressed");
+		actionMapPause.put("escapePressed", new KeyHandler("escape"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("W"), "wPressed");
+		actionMapPause.put("wPressed", new KeyHandler("w"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("A"), "aPressed");
+		actionMapPause.put("aPressed", new KeyHandler("a"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("D"), "dPressed");
+		actionMapPause.put("dPressed", new KeyHandler("d"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("S"), "sPressed");
+		actionMapPause.put("sPressed", new KeyHandler("s"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("SPACE"), "spacePressed");
+		actionMapPause.put("spacePressed", new KeyHandler("space"));
 	}
 	
 	//Nested class to implement KeyBindings
@@ -79,53 +140,92 @@ public class GamePanel extends JPanel implements Runnable{
 			}
 			else if(name == "w"){
 				System.out.println("W Pressed");
+				if(gameEngine.isMovement()){ //While game is running
+					/*
+					 * Depends on how we handle map there should be an if statement that decides goUp() or jump().
+					 * if(gameEngine.getMapObjects(gameEngine.getPlayer().getX(), gameEngine.getPlayer().getY()) instanceOf Ladder &&
+					 * gameEngine.getMapObjects(gameEngine.getPlayer().getX() + 1, gameEngine.getPlayer().getY() + 1) instanceOf Ladder ){
+					 * 	goUp();
+					 * }
+					 * else{
+					 *  jump();
+					 * }
+					 */
+					goUp();
+					System.out.println("Go Up");
+					
+					jump();
+					System.out.println("Jump");
+				}
 			}
 			else if(name == "a"){
 				System.out.println("A Pressed");
+				if(gameEngine.isMovement()){ //While game is running
+					goLeft();
+					System.out.println("Go Left");
+				}
 			}
 			else if(name == "s"){
 				System.out.println("S Pressed");
+				if(gameEngine.isMovement()){ //While game is running
+					goDown();
+					System.out.println("Go Down");
+				}
 			}
 			else if(name == "d"){
 				System.out.println("D Pressed");
+				if(gameEngine.isMovement()){ //While game is running
+					goRight();
+					System.out.println("Go Right");
+				}
+			}
+			else if(name == "space"){
+				System.out.println("Space Pressed");
+				/*
+				 * Player can use space button if and only if he/she has hammer buff.
+				 * Therefore we need an if statement to decide player can use strike action
+				 * if(gameEngine.getPlayer().canStrike()){
+				 *  strike();
+				 * }
+				 */
+				if(gameEngine.isMovement()){ //While game is running
+					strike();
+					System.out.println("Strike");
+				}
 			}
 			
 		}
 		
 	}
 	
-	public void initializeKeyBindings(){
-		InputMap inputMapPause = this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-		ActionMap actionMapPause = this.getActionMap();
+	private void goUp(){
 		
+	}
+	
+	private void jump(){
+		
+	}
+	
+	private void goLeft(){
+		
+	}
+	
+	private void goDown(){
+		
+	}
+	
+	private void goRight(){
+		
+	}
+	
+	private void strike(){
 		/*
-		 * 1st parameter is the name of the keyboard input.
-		 * ESCAPE = ESC button
-		 * W = W button
-		 * A = A button
-		 * S = S button
-		 * D = D button
-		 * 2nd parameter is the name of the action which indicates pressing a button which we will use with ActionMap object.
+		 * We increment current score in this method.
+		 * if(gameEngine.getMapObjects(gameEngine.getPlayer().getX() + 1, gameEngine.getPlayer().getY() + 1) instanceOf Ladder){
+		 * 	score = score + 100;
+		 * }
 		 */
-		
-		/* 
-		 * I decided to use release for ESCAPE to prevent overlapping.
-		 * I want player to press once to pause or resume the game and released ia a perfect fit for that action.
-		 */
-		inputMapPause.put(KeyStroke.getKeyStroke("released ESCAPE"), "escapePressed");
-		actionMapPause.put("escapePressed", new KeyHandler("escape"));
-		
-		inputMapPause.put(KeyStroke.getKeyStroke("W"), "wPressed");
-		actionMapPause.put("wPressed", new KeyHandler("w"));
-		
-		inputMapPause.put(KeyStroke.getKeyStroke("A"), "aPressed");
-		actionMapPause.put("aPressed", new KeyHandler("a"));
-		
-		inputMapPause.put(KeyStroke.getKeyStroke("D"), "dPressed");
-		actionMapPause.put("dPressed", new KeyHandler("d"));
-		
-		inputMapPause.put(KeyStroke.getKeyStroke("S"), "sPressed");
-		actionMapPause.put("sPressed", new KeyHandler("s"));
+		System.out.println("Score is updated");
 	}
 	
 	private void start(){
@@ -143,6 +243,7 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	private void stop(){
 		gameEngine.setGameOver(true);
+		gameEngine.setMovement(false);
 	}
 	
 }
