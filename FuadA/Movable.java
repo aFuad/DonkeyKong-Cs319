e579902c3d6fbb78ec4114ec815d3fdcf7e82fe8
Fuad Ahmad <ahmedovfuad@gmail.com>
2017-11-04 05:59:20 +0300

    Add files via upload

---
+++ FuadA/Movable.java
@@ -0,0 +1,8 @@
+
+public interface Movable {
+	public void goRight();
+	public void goLeft();
+	public void operation();
+	public void setDirection(Direction a);
+	public Direction getDirection();
+}
---
+++ FuadA/enemyType.java
@@ -0,0 +1,6 @@
+
+public enum enemyType {
+	RollingBarrel,
+	FallingBarrel,
+	FireElemental,
+}
