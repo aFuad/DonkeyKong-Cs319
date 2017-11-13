97d3653bfc630355e3116bfec88938e7492eb500
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2017-11-13 20:36:46 +0300

    Update Ladder.java

--- src/Ladder.java
+++ src/Ladder.java
@@ -4,8 +4,8 @@ import java.awt.Image;
 
 import javax.swing.ImageIcon;
 
-public class Ladder extends MyObject implements Nonmovable{
-	private Image imageLadder =  new ImageIcon(this.getClass().getResource("ladder.png")).getImage();
+public class Ladder extends Nonmovable{
+	private Image imageLadder =  new ImageIcon("src/image/ladder.png").getImage();
 	
 	private final boolean PASS_THROUGH = true;
 	
@@ -13,20 +13,10 @@ public class Ladder extends MyObject implements Nonmovable{
 		super(x, y);
 	}
 
+	@Override
 	public boolean getPassThrough(){
+		// TODO Auto-generated method stub
 		return PASS_THROUGH;
-	}
-	
-	@Override
-	public int getX() {
-		// TODO Auto-generated method stub
-		return super.getX();
-	}
-	
-	@Override
-	public int getY() {
-		// TODO Auto-generated method stub
-		return super.getY();
 	}
 
 	public Image getImage() {
