65463bb508a7f5214210a502361a2433d3e39df4
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2017-11-13 20:36:31 +0300

    Update Hammer.java

--- src/Hammer.java
+++ src/Hammer.java
@@ -4,8 +4,8 @@ import java.awt.Image;
 
 import javax.swing.ImageIcon;
 
-public class Hammer extends MyObject implements Nonmovable{
-	private Image imageHammer =  new ImageIcon(this.getClass().getResource("hammer.png")).getImage();
+public class Hammer extends Nonmovable{
+	private Image imageHammer =  new ImageIcon("src/image/hammer.png").getImage();
 	
 	private final boolean PASS_THROUGH = true;
 	
@@ -13,20 +13,10 @@ public class Hammer extends MyObject implements Nonmovable{
 		super(x, y);
 	}
 
+	@Override
 	public boolean getPassThrough(){
+		// TODO Auto-generated method stub
 		return PASS_THROUGH;
-	}
-	
-	@Override
-	public int getX() {
-		// TODO Auto-generated method stub
-		return super.getX();
-	}
-	
-	@Override
-	public int getY() {
-		// TODO Auto-generated method stub
-		return super.getY();
 	}
 
 	public Image getImage() {
