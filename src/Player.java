7aa8b2508d9daf0914119e7f0f4a93b4f7d50627
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2017-11-13 20:40:41 +0300

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,18 @@
 package source;
 
 import java.awt.Image;
+import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
 
 public class Player extends MyObject implements Movable{
-	private Image imagePlayerRight = new ImageIcon(this.getClass().getResource("right.png")).getImage();
-	private Image imagePlayerLeft = new ImageIcon(getClass().getResource("left.png")).getImage();
-	private Image imagePlayerStrike = new ImageIcon(getClass().getResource("strike.png")).getImage();
-	private Image imageJumpRight = new ImageIcon(getClass().getResource("jumpRight.png")).getImage();
-	private Image imageJumpLeft = new ImageIcon(getClass().getResource("jumpLeft.png")).getImage();
-	private Image imagePlayerClimb1= new ImageIcon(getClass().getResource("climb1.png")).getImage();
-	private Image imagePlayerClimb2 = new ImageIcon(getClass().getResource("climb2.png")).getImage();
+	private Image imagePlayerRight = new ImageIcon("src/image/player.png").getImage();
+	private Image imagePlayerLeft = new ImageIcon("src/image/player.png").getImage();
+	private Image imagePlayerStrike = new ImageIcon("src/image/player.png").getImage();
+	private Image imageJumpRight = new ImageIcon("src/image/player.png").getImage();
+	private Image imageJumpLeft = new ImageIcon("src/image/player.png").getImage();
+	private Image imagePlayerClimb1= new ImageIcon("src/image/player.png").getImage();
+	private Image imagePlayerClimb2 = new ImageIcon("src/image/player.png").getImage();
 	
 	private Direction direction;
 
@@ -48,22 +49,30 @@ public class Player extends MyObject implements Movable{
 		return imagePlayerClimb2;
 	}
 
+	/*
+	 * Yes, that's right! Go up decreasing horizontal component of the player.
+	 * Similarly Go down do same thing but reverse.
+	 */
 	public void goUp(){
-		setY(getY() + 1);
+		setY(getY() - 5);
+		relocateRectangle(getX(), getY());
 	}
 	
 	public void goDown(){
-		setY(getY() - 1);
+		setY(getY() + 5);
+		relocateRectangle(getX(), getY());
 	}
 	
 	//We cannot write a jump function in GameEngine Class or GamePanel Class
 	
 	public void goRight(){
-		setX(getX()+1);
+		setX(getX() + 5);
+		relocateRectangle(getX(), getY());
 	}
 	
 	public void goLeft(){
-		setX(getX()-1);
+		setX(getX() - 5);
+		relocateRectangle(getX(), getY());
 	}
 	
 	public void setDirection(Direction direction){
@@ -72,5 +81,10 @@ public class Player extends MyObject implements Movable{
 	
 	public Direction getDirection(){
 		return direction;
+	}
+	
+	public void falling(){
+		setY(getY() + 10);
+		relocateRectangle(getX(), getY());
 	}
 }
