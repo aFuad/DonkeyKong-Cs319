276c0a1a4f635a75ff86eaf100f17fb6a9656ee8
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2017-11-13 20:29:36 +0300

    Update Main.java

--- cKupeli/workspace/Donkey Kong/src/source/Main.java
+++ cKupeli/workspace/Donkey Kong/src/source/Main.java
@@ -2,18 +2,12 @@ package source;
 
 import java.io.FileNotFoundException;
 
-public class Main implements Runnable{
+public class Main{
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		
 		MainFrame frame = new MainFrame();
 		frame.setVisible(true);
-	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
 	}
 }
