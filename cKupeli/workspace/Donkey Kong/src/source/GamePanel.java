423f8f963bbc60447908803ab132fd8a2980552e
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2017-11-28 16:40:43 +0300

    Keybindings bug fixes, jump and climb

--- cKupeli/workspace/Donkey Kong/src/source/GamePanel.java
+++ cKupeli/workspace/Donkey Kong/src/source/GamePanel.java
@@ -1,8 +1,6 @@
 package source;
 
-import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.io.FileNotFoundException;
 
@@ -16,6 +14,8 @@ import javax.swing.KeyStroke;
 public class GamePanel extends JPanel implements Runnable{
 	private Thread myThread;
 	private GameEngine gameEngine;
+	
+	private boolean buttonW, buttonA, buttonS, buttonD, buttonSpace = false;
 
 	public GamePanel(int level) throws FileNotFoundException{
 		init(level);
@@ -41,8 +41,32 @@ public class GamePanel extends JPanel implements Runnable{
 		// TODO Auto-generated method stub
 		while(!gameEngine.isGameOver()){ //While game is not over
 			if(gameEngine.isMovement()){ //While game is running
-				//Draw the game
-				gameEngine.sPressed(); //Gravity
+				//Keybindings boolean values
+				if(buttonW){
+					gameEngine.wPressed();
+				}
+				
+				if(buttonA){
+					gameEngine.aPressed();
+				}
+				
+				if(buttonS){
+					gameEngine.sPressed();
+				}
+				
+				if(buttonD){
+					gameEngine.dPressed();
+				}
+				
+				if(buttonSpace){
+					gameEngine.spacePressed();
+				}
+				
+				if(gameEngine.isJump()){
+					gameEngine.jump();
+					//System.out.println(gameEngine.isJump());
+				}
+				gameEngine.gravity();
 			}
 			else if(!gameEngine.isMovement()){ //While game is paused
 				//Pause menu initialization if needed
@@ -51,7 +75,7 @@ public class GamePanel extends JPanel implements Runnable{
 			//gameEngine.sPressed();
 			
 			try{
-				Thread.sleep(100); //Rendering speed of the thread
+				Thread.sleep(20); //Rendering speed of the thread 100
 			}
 			catch (InterruptedException e){
 				// TODO Auto-generated catch block
@@ -73,8 +97,8 @@ public class GamePanel extends JPanel implements Runnable{
 		super.paintComponent(g);
 		//setBackground(Color.BLACK);
 		//Test
-		for(int y = 0; y < 15; y++){
-			for(int x = 0; x < 15; x++){
+		for(int y = 0; y < 20; y++){
+			for(int x = 0; x < 20; x++){
 				//Commented part below aim to test rectangles of nonmovable objects
 				if(gameEngine.getMapObject(x, y) instanceof Platform){
 					g.drawImage(gameEngine.getMapObject(x, y).getImage(), gameEngine.getMapObject(x, y).getX(), gameEngine.getMapObject(x, y).getY(), this);
@@ -84,7 +108,7 @@ public class GamePanel extends JPanel implements Runnable{
 					g.drawImage(gameEngine.getMapObject(x, y).getImage(), gameEngine.getMapObject(x, y).getX(), gameEngine.getMapObject(x, y).getY(), this);
 					//g.drawRect((int) gameEngine.getMapObject(x, y).getRectangle().getMinX(), (int) gameEngine.getMapObject(x, y).getRectangle().getMinY(), (int) gameEngine.getMapObject(x, y).getRectangle().getMaxX() - (int) gameEngine.getMapObject(x, y).getRectangle().getMinX(), (int) gameEngine.getMapObject(x, y).getRectangle().getMaxY() - (int) gameEngine.getMapObject(x, y).getRectangle().getMinY());
 				}
-				else if(y < 14){ //If y = 14, then inside the if statement we check y = 15 and program gives outOfBound error
+				else if(y < 19){ //If y = 19, then inside the if statement we check y = 20 and program gives outOfBound error
 					if(gameEngine.getMapObject(x, y) instanceof Girl && gameEngine.getMapObject(x, y + 1) instanceof Girl){ //To create girl, we need 2 blocks because monkey takes 50x100 space
 						g.drawImage(gameEngine.getMapObject(x, y).getImage(), gameEngine.getMapObject(x, y).getX(), gameEngine.getMapObject(x, y).getY(), this);
 						//g.drawRect((int) gameEngine.getMapObject(x, y).getRectangle().getMinX(), (int) gameEngine.getMapObject(x, y).getRectangle().getMinY(), (int) gameEngine.getMapObject(x, y).getRectangle().getMaxX() - (int) gameEngine.getMapObject(x, y).getRectangle().getMinX(), (int) gameEngine.getMapObject(x, y).getRectangle().getMaxY() - (int) gameEngine.getMapObject(x, y).getRectangle().getMinY());
@@ -93,7 +117,7 @@ public class GamePanel extends JPanel implements Runnable{
 						g.drawImage(gameEngine.getMapObject(x, y).getImage(), gameEngine.getMapObject(x, y).getX(), gameEngine.getMapObject(x, y).getY(), this);
 						//g.drawRect((int) gameEngine.getMapObject(x, y).getRectangle().getMinX(), (int) gameEngine.getMapObject(x, y).getRectangle().getMinY(), (int) gameEngine.getMapObject(x, y).getRectangle().getMaxX() - (int) gameEngine.getMapObject(x, y).getRectangle().getMinX(), (int) gameEngine.getMapObject(x, y).getRectangle().getMaxY() - (int) gameEngine.getMapObject(x, y).getRectangle().getMinY());
 					}
-					else if(x < 14){ //If x = 14, then inside the if statement we check x = 15 and program gives outOfBound error.
+					else if(x < 19){ //If x = 19, then inside the if statement we check x = 20 and program gives outOfBound error.
 						//To create monkey, we need 4 blocks because monkey takes 100x100 space
 						if(gameEngine.getMapObject(x, y) instanceof Monkey && gameEngine.getMapObject(x + 1, y) instanceof Monkey && gameEngine.getMapObject(x, y + 1) instanceof Monkey && gameEngine.getMapObject(x + 1, y + 1) instanceof Monkey){
 							g.drawImage(gameEngine.getMapObject(x, y).getImage(), gameEngine.getMapObject(x, y).getX(), gameEngine.getMapObject(x, y).getY(), this);
@@ -128,23 +152,38 @@ public class GamePanel extends JPanel implements Runnable{
 		 * We decided to use release for ESCAPE to prevent overlapping.
 		 * We want player to press once to pause or resume the game and released is a perfect fit for that action.
 		 */
-		inputMapPause.put(KeyStroke.getKeyStroke("released ESCAPE"), "escapePressed");
-		actionMapPause.put("escapePressed", new KeyHandler("escape"));
+		inputMapPause.put(KeyStroke.getKeyStroke("released ESCAPE"), "escapeReleased");
+		actionMapPause.put("escapeReleased", new KeyHandler("escape"));
 		
 		inputMapPause.put(KeyStroke.getKeyStroke("W"), "wPressed");
-		actionMapPause.put("wPressed", new KeyHandler("w"));
+		actionMapPause.put("wPressed", new KeyHandler("wPressed"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("released W"), "wReleased");
+		actionMapPause.put("wReleased", new KeyHandler("wReleased"));
 		
 		inputMapPause.put(KeyStroke.getKeyStroke("A"), "aPressed");
-		actionMapPause.put("aPressed", new KeyHandler("a"));
+		actionMapPause.put("aPressed", new KeyHandler("aPressed"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("released A"), "aReleased");
+		actionMapPause.put("aReleased", new KeyHandler("aReleased"));
 		
 		inputMapPause.put(KeyStroke.getKeyStroke("D"), "dPressed");
-		actionMapPause.put("dPressed", new KeyHandler("d"));
+		actionMapPause.put("dPressed", new KeyHandler("dPressed"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("released D"), "dReleased");
+		actionMapPause.put("dReleased", new KeyHandler("dReleased"));
 		
 		inputMapPause.put(KeyStroke.getKeyStroke("S"), "sPressed");
-		actionMapPause.put("sPressed", new KeyHandler("s"));
+		actionMapPause.put("sPressed", new KeyHandler("sPressed"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("released S"), "sReleased");
+		actionMapPause.put("sReleased", new KeyHandler("sReleased"));
 		
 		inputMapPause.put(KeyStroke.getKeyStroke("SPACE"), "spacePressed");
-		actionMapPause.put("spacePressed", new KeyHandler("space"));
+		actionMapPause.put("spacePressed", new KeyHandler("spacePressed"));
+		
+		inputMapPause.put(KeyStroke.getKeyStroke("released SPACE"), "spaceReleased");
+		actionMapPause.put("spaceReleased", new KeyHandler("spaceReleased"));
 	}
 	
 	//Nested class to implement KeyBindings
@@ -162,38 +201,59 @@ public class GamePanel extends JPanel implements Runnable{
 				System.out.println("Escape Pressed");
 				if(gameEngine.isMovement()){ //While game is running
 					pause();
-					System.out.print("Movement is ");
-					System.out.println(gameEngine.isMovement());
 				}
 				else if(!gameEngine.isMovement()){ //While game is paused
 					resume();
-					System.out.print("Movement is ");
-					System.out.println(gameEngine.isMovement());
 				}
 			}
-			else if(name == "w"){
+			else if(name == "wPressed"){
 				if(gameEngine.isMovement()){ //While game is running
-					gameEngine.wPressed();
+					buttonW = true;
 				}
 			}
-			else if(name == "a"){
+			else if(name == "wReleased"){
 				if(gameEngine.isMovement()){ //While game is running
-					gameEngine.aPressed();
+					buttonW = false;
 				}
 			}
-			else if(name == "s"){
+			else if(name == "aPressed"){
 				if(gameEngine.isMovement()){ //While game is running
-					gameEngine.sPressed();
+					buttonA = true;
 				}
 			}
-			else if(name == "d"){
+			else if(name == "aReleased"){
 				if(gameEngine.isMovement()){ //While game is running
-					gameEngine.dPressed();
+					buttonA = false;
 				}
 			}
-			else if(name == "space"){
+			else if(name == "sPressed"){
 				if(gameEngine.isMovement()){ //While game is running
-					gameEngine.spacePressed();
+					buttonS = true;
+				}
+			}
+			else if(name == "sReleased"){
+				if(gameEngine.isMovement()){ //While game is running
+					buttonS = false;
+				}
+			}
+			else if(name == "dPressed"){
+				if(gameEngine.isMovement()){ //While game is running
+					buttonD = true;
+				}
+			}
+			else if(name == "dReleased"){
+				if(gameEngine.isMovement()){ //While game is running
+					buttonD = false;
+				}
+			}
+			else if(name == "spacePressed"){
+				if(gameEngine.isMovement()){ //While game is running
+					buttonSpace = true;
+				}
+			}
+			else if(name == "spaceReleased"){
+				if(gameEngine.isMovement()){ //While game is running
+					buttonSpace = false;
 				}
 			}
 			
