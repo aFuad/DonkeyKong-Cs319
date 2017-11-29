d6163fd52023d9d86c1955176294cafe525d2933
Çağatay Küpeli <32224283+ckupeli@users.noreply.github.com>
2017-11-29 16:58:04 +0300

    Update MainFrame.java

--- cKupeli/workspace/Donkey Kong/src/source/MainFrame.java
+++ cKupeli/workspace/Donkey Kong/src/source/MainFrame.java
@@ -1,30 +1,18 @@
 package source;
 
-import java.io.FileNotFoundException;
-
 import javax.swing.JFrame;
-import javax.swing.border.EmptyBorder;
 
-public class MainFrame extends JFrame{
-	
-	protected static final int WIDTH = 1000;
-	protected static final int HEIGHT = 1000;
-	private static final String TITLE = "Donkey Kong";
-	private int level = 1;
-	
-	GamePanel gamePanel;
-	MainMenu mainMenu;
-	
-	public MainFrame() throws FileNotFoundException{
-		setTitle(TITLE);
-		setBounds(600, 100, WIDTH, HEIGHT);
-		setResizable(false);
-		gamePanel = new GamePanel(level);
-		gamePanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(gamePanel);
-		gamePanel.setLayout(null);
-		//mainMenu = new MainMenu();
-		//mainMenu.setBorder(new EmptyBorder(5, 5, 5, 5));
-		//setContentPane(mainMenu);
+public class MainFrame {
+
+	public static void main(String[] args) {
+		//JFrame frame = new JFrame("Donkey Kong");
+		GUIPanelManager guiManager = new GUIPanelManager();
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.getContentPane().add(new MainMenu(guiManager));
+		//frame.setResizable(false);
+		//frame.pack();
+		//frame.setVisible(true);
+
 	}
+
 }
